15a7a1fbaf50ae6b186c782427fc49b98b3ae2f1
Alex Gusev <alex@flancer64.com>
2015-10-08 20:23:09 +0300

    MOBI-100 - CRUD для M1 и M2

---
+++ src/Entity/Bonus/Type.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2015, Praxigento
+ * All rights reserved.
+ */
+
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Flancer32\Lib\Entity\Bonus;
+
+
+class Type {
+    const ATTR_ID = 'id';
+    const ATTR_NOTE = 'note';
+    const ATTR_VALUE = 'value';
+    const NAME = 'fl32_sample_bonus_type';
+}
