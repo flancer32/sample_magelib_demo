009355c1d3ec16570116f84ed9b4acbd14c2de8f
Alex Gusev <alex@flancer64.com>
2015-10-22 15:36:01 +0300

    MOBI-104 - Setup Travis for M1 & M2 generics

--- src/Context.php
+++ src/Context.php
@@ -1,31 +1,42 @@
 <?php
 /**
- * Copyright (c) 2015, Praxigento
- * All rights reserved.
- */
-
-/**
+ * Execution environment context (Magento 1 / Magento 2).
+ *
  * User: Alex Gusev <alex@flancer64.com>
  */
-
 namespace Flancer32\Lib;
 
-/**
- * Library's context
- * Class Context
- * @package Flancer32\Lib
- */
 class Context {
+    /**
+     * Type of the execution environment.
+     */
     const TYPE_MAGE_1 = 'M1';
     const TYPE_MAGE_2 = 'M2';
     const TYPE_TEST = 'TEST_UNIT';
-    private $_contextType = self::TYPE_TEST;
-    /** @var string TODO: remove this tmp code */
-    private $_dbPrefix = 'm1_';
     /**
+     * Flag for current execution context type.
+     * @var string
+     */
+    private $_contextType = self::TYPE_TEST;
+    /**
+     * Saved database connection.
      * @var \Zend_Db_Adapter_Pdo_Mysql
      */
     private $_connection;
+    /**
+     * Schema setup for M2 to get table names in CLI mode (./bin/magento as Symfony CLI app)
+     * @var \Magento\Framework\Setup\SchemaSetupInterface
+     */
+    private $_setupSchema;
+    /** @var \Magento\Framework\ObjectManagerInterface */
+    private $_m2ObjectManager;
+    /** @var  \Magento\Framework\App\Resource */
+    private $_m2Resource;
+    /**
+     * Resource setup for M1 to get table names.
+     * @var \Mage_Core_Model_Resource_Setup
+     */
+    private $_setupResource;
     /**
      * Itself. Singleton.
      *
@@ -40,13 +51,19 @@ class Context {
      */
     public function __construct() {
         /* analyze current runtime environment Magento 1 | Magento 2 | TestUnits (default) */
-        if(class_exists('Mage', false)) {
+        if(class_exists('\Mage', false)) {
             $this->_contextType = self::TYPE_MAGE_1;
+            $resource = \Mage::getModel('core/resource_setup');
+            $this->setSetupResource($resource);
         } elseif(
             isset($GLOBALS['bootstrap']) &&
             ($GLOBALS['bootstrap'] instanceof \Magento\Framework\App\Bootstrap)
         ) {
             $this->_contextType = self::TYPE_MAGE_2;
+            /** @var  $bootstrap \Magento\Framework\App\Bootstrap */
+            $bootstrap = $this->getBootstrap();
+            $this->_m2ObjectManager = $bootstrap->getObjectManager();
+            $this->_m2Resource = $this->_m2ObjectManager->get('\Magento\Framework\App\Resource');
         }
     }
 
@@ -63,6 +80,27 @@ class Context {
     }
 
     /**
+     * Set test unit instance with mocked methods.
+     *
+     * @param Context $instance
+     */
+    public static function  set(Context $instance) {
+        self::$_instance = $instance;
+    }
+
+    /**
+     * Reset instance.
+     */
+    public static function  reset() {
+        self::$_instance = null;
+    }
+
+    public function isMage2() {
+        $result = ($this->_contextType == self::TYPE_MAGE_2);
+        return $result;
+    }
+
+    /**
      * Setup database connection.
      *
      * @param \Zend_Db_Adapter_Pdo_Mysql $val
@@ -75,23 +113,34 @@ class Context {
      * Connection to database.
      * @return \Zend_Db_Adapter_Pdo_Mysql
      */
-    public function getConnection() {
+    public function getConnection($type = 'core_write') {
         if(!$this->_connection instanceof \Zend_Db_Adapter_Abstract) {
             if($this->_contextType == self::TYPE_MAGE_1) {
-                $this->_connection = \Mage::getSingleton('core/resource')->getConnection('core_write');
+                $this->_connection = \Mage::getSingleton('core/resource')->getConnection($type);
             } elseif($this->_contextType == self::TYPE_MAGE_2) {
-                /** @var  $bootstrap \Magento\Framework\App\Bootstrap */
-                $bootstrap = $GLOBALS['bootstrap'];
-                $object_manager = $bootstrap->getObjectManager();
-                /** @var  $appRsrc \Magento\Framework\App\Resource */
-                $appRsrc = $object_manager->get('\Magento\Framework\App\Resource');
-                $this->_connection = $appRsrc->getConnection('core_write');
-                $this->_dbPrefix = '';
+                $this->_connection = $this->_m2Resource->getConnection($type);
             } else {
                 throw new \Exception('Database connection is not set in the context. You should setup connection manually in PHPUnit tests.');
             }
         }
         return $this->_connection;
+    }
+
+    /**
+     * @return \Magento\Framework\App\Bootstrap
+     */
+    private function getBootstrap() {
+        $result = $GLOBALS['bootstrap'];
+        return $result;
+    }
+
+    public function getObjectManager() {
+        $result = null;
+        if($this->_contextType == self::TYPE_MAGE_2) {
+            $result = $this->getBootstrap()->getObjectManager();
+        } else {
+        }
+        return $result;
     }
 
     /**
@@ -102,7 +151,38 @@ class Context {
      * @return string
      */
     public function getTableName($entityName) {
-        $result = $this->_dbPrefix . $entityName;
+        if($this->_setupResource) {
+            /* M1 setup */
+            $result = $this->_setupResource->getTable($entityName);
+        } elseif($this->_setupSchema) {
+            /* M2 setup/CLI*/
+            $result = $this->_setupSchema->getTable($entityName);
+        } elseif($this->_m2Resource) {
+            /* M2 tests/app */
+            $result = $this->_m2Resource->getTableName($entityName);
+        } else {
+            $result = $entityName;
+        }
         return $result;
+    }
+
+    /**
+     * Set schema setup object in Symfony CLI app.
+     *
+     * @param \Magento\Framework\Setup\SchemaSetupInterface $val
+     */
+    public function setSetupSchema(\Magento\Framework\Setup\SchemaSetupInterface $val) {
+        $this->_setupSchema = $val;
+        $this->_connection = $val->getConnection();
+    }
+
+    /**
+     * Set resource setup object in M1 app.
+     *
+     * @param \Mage_Core_Model_Resource_Setup $val
+     */
+    public function setSetupResource(\Mage_Core_Model_Resource_Setup $val) {
+        $this->_setupResource = $val;
+        $this->_connection = $val->getConnection();
     }
 }
